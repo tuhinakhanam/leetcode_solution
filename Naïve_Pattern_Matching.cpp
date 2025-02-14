6ce2ae66568775ee5a3a40a2495127caf6ab4ce9
tuhinakhanam <tuhinakhanam1@gmail.com>
2025-02-14 12:34:16 +0530

    Add naive pattern matching implementation

---
+++ Naïve_Pattern_Matching.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void naiveSearch(string text, string pattern)
+{
+    int n = text.length();
+    int m = pattern.length();
+
+    for (int i = 0; i <= n - m; i++)
+    {
+        int j;
+        for (j = 0; j < m; j++)
+        {
+            if (text[i + j] != pattern[j])
+                break;
+        }
+        if (j == m)
+        {
+            cout << "Pattern found at index " << i << endl;
+        }
+    }
+}
+
+int main()
+{
+    string text, pattern;
+    cin >> text;
+    cin >> pattern;
+
+
+    naiveSearch(text, pattern);
+
+    return 0;
+}
